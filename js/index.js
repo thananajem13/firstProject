24a2471cc632d6b27cc2b41e7dda35e378db81ed
Asmaa-Nabil <asmaanabil6699@gmail.com>
2022-05-13 10:17:10 +0200

    Asmaa update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 function getName() {
     alert(`thana' najem`);
 }
+
+function cutEmail(){
+    var emailInput = document.getElementById("email");
+    var nameInput = document.getElementById("name");
+    var domainInput = document.getElementById("domain");
+
+    if(emailInput.value.includes("@")){
+        var indexOfAt = emailInput.value.indexOf("@");
+        var nameOfEmail = emailInput.value.slice(0, indexOfAt);
+        nameInput.value = nameOfEmail;
+        domainInput.value = emailInput.value.slice(indexOfAt+1);
+    }
+    else{
+        alert("Please input a corrct email")
+    }
+}
